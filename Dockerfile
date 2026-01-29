e78a766f60efe37858ca48c1932c432115db5733
Pakin <pakin@mfec.co.th>
2026-01-29 13:32:15 +0700

    feat:  enhance GCS upload configuration with service accounts, and update Docker environments with security and logging improvements.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Production Dockerfile for Strapi v5
 
 # Build Stage
-FROM node:20-alpine AS build
+FROM node:25-alpine AS build
 # Installing libvips-dev for sharp Compatibility
 RUN apk update && apk add --no-cache build-base gcc autoconf automake zlib-dev libpng-dev vips-dev > /dev/null 2>&1
 ARG NODE_ENV=production
@@ -14,13 +14,12 @@ RUN npm config set fetch-retry-maxtimeout 600000 -g && npm install --production=
 ENV PATH /opt/node_modules/.bin:$PATH
 WORKDIR /opt/app
 COPY . .
-ENV NODE_OPTIONS="--max-old-space-size=4096"
-ENV GENERATE_SOURCEMAP=false
+
 RUN npm run build
 RUN npm prune --production
 
 # Production Stage
-FROM node:20-alpine
+FROM node:25-alpine
 RUN apk add --no-cache vips-dev
 ARG NODE_ENV=production
 ENV NODE_ENV=${NODE_ENV}
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,25 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
-      providerOptions: {
-        bucketName: env('GCS_BUCKET_NAME'), // Your bucket name
-        publicFiles: env.bool('GCS_PUBLIC_FILES', false), // Set to true if you want public URLs
-        uniform: env.bool('GCS_UNIFORM_ACL', false), // Set to true if your bucket uses uniform bucket-level access
-        basePath: env('GCS_BASE_PATH', ''), // Optional sub-folder
-        serviceAccount: JSON.parse(require('fs').readFileSync(env('GCS_SERVICE_ACCOUNT_KEY_PATH'), 'utf-8')), // Your service account JSON key
+import path from "path";
+import fs from "fs";
+
+module.exports = ({ env }) => {
+
+  console.log("serviceAccount path", env("GCS_SERVICE_ACCOUNT_KEY_PATH"));
+  return {
+    upload: {
+      config: {
+        provider:
+          "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: env("GCS_BUCKET_NAME"),
+          publicFiles: env.bool("GCS_PUBLIC_FILES", false),
+          uniform: env.bool("GCS_UNIFORM_ACL", false),
+          serviceAccount: fs.existsSync(env("GCS_SERVICE_ACCOUNT_KEY_PATH"))
+            ? JSON.parse(
+                fs.readFileSync(env("GCS_SERVICE_ACCOUNT_KEY_PATH"), "utf-8"),
+              )
+            : undefined,
+        },
       },
     },
-  },
-});
+  };
+};
